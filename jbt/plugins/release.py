584f45d453ed7186d441eb38f8d3e277a76d4a2c
maybejake <jake@cow.cool>
2026-05-11 11:34:25 +0100

    Beetify

---
+++ jbt/plugins/release.py
@@ -0,0 +1,60 @@
+from beet import (
+    Context,
+    Language
+)
+import logging
+import tempfile
+import shutil
+import os
+from pathlib import Path
+
+LOGGER = logging.getLogger(__name__)
+
+
+def beet_default(ctx: Context):
+    """Package the datapack for release"""
+    # Get git tag if available
+    ref = os.getenv("GITHUB_REF_NAME")
+    ref_type = os.getenv("GITHUB_REF_TYPE")
+
+    # Use project version, unless git tag exists
+    version = f"v{ctx.project_version}"
+    if ref_type == "tag":
+        version = ref
+
+    # Set version
+    lang = ctx.assets.languages["jbt:en_us"].data
+    lang["jbt.version"] = version
+    ctx.assets["jbt:en_us"] = Language(lang)
+
+    # Save datapack
+    ctx.data.save(
+        path=ctx.directory / ".build" / f"{ctx.project_name}-{version}-Data-Pack",
+        overwrite=True,
+        zipped=True
+    )
+
+    # Save resourcepack
+    ctx.assets.save(
+        path=ctx.directory / ".build" / f"{ctx.project_name}-{version}-Resource-Pack",
+        overwrite=True,
+        zipped=True
+    )
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_path = Path(temp_dir)
+
+        # Save datapack
+        data_pack_dir = temp_path / "data-pack"
+        ctx.data.save(path=data_pack_dir)
+
+        # Save resource pack
+        resource_pack_dir = temp_path / "resource-pack"
+        ctx.assets.save(path=resource_pack_dir)
+
+        # Place data folder into resourcepack
+        shutil.copytree(data_pack_dir / "data", resource_pack_dir / "data", dirs_exist_ok=True)
+
+        # Zip
+        data_resource_pack_dir = ctx.directory / ".build" / f"{ctx.project_name}-{version}-Data-Resource-Pack"
+        shutil.make_archive(data_resource_pack_dir, "zip", resource_pack_dir)
